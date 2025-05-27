3631ec2f7204101ecafaf3899f30ffd8be811cd8
rukhsana1827 <rukhsana1827asif@gmail.com>
2025-05-27 18:11:09 +0530

    Initial commit

---
+++ src/test/java/com/Java/Vitoxyz/Backend/VitoxyzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Java.Vitoxyz.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitoxyzBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
